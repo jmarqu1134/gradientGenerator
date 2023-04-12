74b9accf7b7df94bd90a41f5d587197d7f1a29c9
Jesse Marquez <archares@Jesses-Air.lan>
2023-04-12 01:29:29 -0700

    Modified the code to show hex and created a function to conver the hex to rgb.

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-var css = document.querySelector("h3");
+var cssRGB = document.querySelector(".rgb");
+var cssHex = document.querySelector(".hex");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
-function setGradient() {
-  body.style.background =
-    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+function hexToRgb(hex) {
+  // Remove '#' if present
+  hex = hex.replace("#", "");
 
-  css.textContent = body.style.background + ";";
+  let r = parseInt(hex.substr(0, 2), 16);
+  let g = parseInt(hex.substr(2, 2), 16);
+  let b = parseInt(hex.substr(4, 2), 16);
+  // Return RGB value as an object
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function setGradient() {
+  let colorString = `${color1.value}, ${color2.value}`;
+  body.style.background = `linear-gradient(to right, ${colorString} )`;
+
+  cssHex.textContent = "Hex conversion: " + color1.value + ", " + color2.value;
+  cssRGB.textContent =
+    "RGB conversion: " + hexToRgb(color1.value) + ", " + hexToRgb(color2.value);
 }
 
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
